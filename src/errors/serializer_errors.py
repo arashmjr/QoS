f8bd20620ce9a482c5a111cc73e6c71c0c9ac500
Arash <arash.mjr@gmail.com>
2022-11-10 08:51:48 +0330

    fix(back): serializer errors

--- src/errors/serializer_errors.py
+++ src/errors/serializer_errors.py
@@ -1,7 +1,17 @@
 class SerializerErrors:
     class CreateReminderSerializer:
         errors = {
+            "title": "CREATE_REMINDER_SERIALIZER_INVALID_TITLE",
             "user": "CREATE_REMINDER_SERIALIZER_INVALID_USER",
+            "message": "CREATE_REMINDER_SERIALIZER_INVALID_MESSAGE",
             "reminder_time": "CREATE_REMINDER_SERIALIZER_INVALID_REMINDER_TIME",
             "threshold": "CREATE_REMINDER_SERIALIZER_INVALID_THRESHOLD",
+        }
+
+    class UpdateReminderSerializer:
+        errors = {
+            "title": "UPDATE_REMINDER_SERIALIZER_INVALID_TITLE",
+            "message": "UPDATE_REMINDER_SERIALIZER_INVALID_MESSAGE",
+            "reminder_time": "UPDATE_REMINDER_SERIALIZER_INVALID_REMINDER_TIME",
+            "threshold": "UPDATE_REMINDER_SERIALIZER_INVALID_THRESHOLD",
         }
