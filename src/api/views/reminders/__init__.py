bd1f6f9a0cc66688a14e4f0178f154de812d51d8
Arash <arash.mjr@gmail.com>
2022-11-10 09:58:36 +0330

    feat(back): delete reminder view

--- src/api/views/reminders/__init__.py
+++ src/api/views/reminders/__init__.py
@@ -1,7 +1,10 @@
 from src.api.views.reminders.create_reminder import CreateReminderAPIView
+from src.api.views.reminders.delete_reminder import DeleteReminderAPIView
 from src.api.views.reminders.retrieve_reminder import RetrieveReminderAPIView
 from src.api.views.reminders.update_reminder import UpdateReminderAPIView
 
 
-class ReminderAPIView(RetrieveReminderAPIView, UpdateReminderAPIView):
+class ReminderAPIView(
+    RetrieveReminderAPIView, UpdateReminderAPIView, DeleteReminderAPIView
+):
     pass
