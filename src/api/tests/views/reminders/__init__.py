585d2a3473390bdccb241464ab0af13adb8b2954
Arash <arash.mjr@gmail.com>
2022-11-10 09:36:52 +0330

    feat(back): retrieve reminder view test

--- src/api/tests/views/reminders/__init__.py
+++ src/api/tests/views/reminders/__init__.py
@@ -1,6 +1,9 @@
 from src.api.tests.views.reminders.create_reminder import (
     CreateReminderAPIViewTestCase,
 )
+from src.api.tests.views.reminders.retrieve_reminder import (
+    RetrieveReminderViewTestCase,
+)
 from src.api.tests.views.reminders.update_reminder import (
     UpdateReminderViewTestCase,
 )
