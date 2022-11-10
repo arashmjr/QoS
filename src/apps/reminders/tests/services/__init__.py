a745be8373c3cbf600dd31a81b2b07562822f121
Arash <arash.mjr@gmail.com>
2022-11-10 09:59:29 +0330

    feat(back): delete reminder service test

--- src/apps/reminders/tests/services/__init__.py
+++ src/apps/reminders/tests/services/__init__.py
@@ -1,3 +1,6 @@
+from src.apps.reminders.tests.services.delete_reminder import (
+    DeleteReminderServiceTestCase,
+)
 from src.apps.reminders.tests.services.get_reminder_by_id import (
     GetReminderByIdServiceTestCase,
 )
