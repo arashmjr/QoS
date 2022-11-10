30a28b4a5838e1dce71fc1b1296eda4fb2160679
Arash <arash.mjr@gmail.com>
2022-11-10 08:49:01 +0330

    feat(back): get reminder by id service with test

--- src/apps/reminders/services/__init__.py
+++ src/apps/reminders/services/__init__.py
@@ -1,1 +1,3 @@
 from src.apps.reminders.services.create_reminder import create_reminder
+from src.apps.reminders.services.get_reminder_by_id import get_reminder_by_id
+from src.apps.reminders.services.update_reminder import update_reminder
---
+++ src/apps/reminders/tests/services/__init__.py
@@ -0,0 +1,6 @@
+from src.apps.reminders.tests.services.get_reminder_by_id import (
+    GetReminderByIdServiceTestCase,
+)
+from src.apps.reminders.tests.services.update_reminder import (
+    UpdateReminderServiceTestCase,
+)
