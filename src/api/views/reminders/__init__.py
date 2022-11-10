77db91f3eff96b7d84179c418a501309bb1ee344
Arash <arash.mjr@gmail.com>
2022-11-10 09:37:07 +0330

    feat(back): retrieve reminder view

--- src/api/views/reminders/__init__.py
+++ src/api/views/reminders/__init__.py
@@ -1,2 +1,7 @@
 from src.api.views.reminders.create_reminder import CreateReminderAPIView
+from src.api.views.reminders.retrieve_reminder import RetrieveReminderAPIView
 from src.api.views.reminders.update_reminder import UpdateReminderAPIView
+
+
+class ReminderAPIView(RetrieveReminderAPIView, UpdateReminderAPIView):
+    pass
