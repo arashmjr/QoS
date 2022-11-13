2906d6f210a340dc21a74bf69e61b2659d379ec8
Arash <arash.mjr@gmail.com>
2022-11-13 10:27:20 +0330

    feat(back): get reminder by userid service with test

--- src/apps/reminders/tests/services/__init__.py
+++ src/apps/reminders/tests/services/__init__.py
@@ -4,6 +4,9 @@ from src.apps.reminders.tests.services.delete_reminder import (
 from src.apps.reminders.tests.services.get_reminder_by_id import (
     GetReminderByIdServiceTestCase,
 )
+from src.apps.reminders.tests.services.get_reminders_by_user_id import (
+    GetRemindersByUserIdServicesTestCase,
+)
 from src.apps.reminders.tests.services.update_reminder import (
     UpdateReminderServiceTestCase,
 )
