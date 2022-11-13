f3211812c012923c7b746be9f89db8672fa133ad
Arash <arash.mjr@gmail.com>
2022-11-13 10:27:51 +0330

    fix(back): create reminder serializer

--- src/apps/reminders/serializers/create_reminder.py
+++ src/apps/reminders/serializers/create_reminder.py
@@ -3,18 +3,11 @@ from src.apps.reminders.models import Reminder
 
 
 class CreateReminderSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField(read_only=True)
-
     class Meta:
         model = Reminder
         fields = (
             "title",
             "user",
-            "message",
-            "image",
             "reminder_time",
             "threshold",
-            "repeat_interval",
-            "created_at",
-            "upadted_at",
         )
