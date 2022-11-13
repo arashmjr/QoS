6110daac89dd7a29d6fd4a08991f5fdb2cd05b25
Arash <arash.mjr@gmail.com>
2022-11-13 10:29:00 +0330

    feat(back): errors

--- src/errors/error_enum.py
+++ src/errors/error_enum.py
@@ -1,2 +1,6 @@
 class ErrorEnum:
-    pass
+    class Reminder:
+        INVALID_LIMIT_AND_OFFSET = "Reminder_INVALID_LIMIT_AND_OFFSET"
+
+    class Service:
+        INVALID_USER_ID = "SERVICE_INVALID_USER_ID"
