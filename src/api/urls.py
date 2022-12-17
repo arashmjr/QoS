fbb7f76379f7067506a4ca71ceda07b691d6dd00
Arash <arash.mjr@gmail.com>
2022-12-17 17:42:41 +0330

    feat(back):url

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
-from src.api.views.website import UploadFileAPIView
+from src.api.views.website import (CalculateDelayAPIView,
+                                   TestDelayFromQueueItems, UploadFileAPIView)
 
 website_url_patterns = [
     path("upload-csv/", UploadFileAPIView.as_view(), name="upload_csv"),
+    path("calculate-delay/", CalculateDelayAPIView.as_view(), name="calculate_delay"),
+    path("test-delay-from-queue/", TestDelayFromQueueItems.as_view(), name="test_delay"),
 ]
 
 V_0_0_0_urlpatterns = [
