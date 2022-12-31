73d5248705dfc48db2e73800b7bffa1c30f652ef
Arash <arash.mjr@gmail.com>
2022-12-31 18:01:58 +0330

    fix(back): remove calculate delay view

--- src/api/views/website/__init__.py
+++ src/api/views/website/__init__.py
@@ -1,5 +1,2 @@
-from src.api.views.website.calculate_delay import CalculateDelayAPIView
-from src.api.views.website.test_delay_queue_items import (
-    TestDelayFromQueueItems,
-)
+from src.api.views.website.test_delay import TestDelayAPIView
 from src.api.views.website.upload_file import UploadFileAPIView
